821e603d6c5a47aebab1e02f1fa83327df04ab87
RenukaAsane <143023481+RenukaAsane@users.noreply.github.com>
2024-06-14 21:59:47 +0530

    Add files via upload

---
+++ session1.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 15 04:12:04 2024
+
+@author: Admin
+"""
+#Print Hello World
+print("Hello World")
+#Variables types
+x=1.5        
+print(x)
+print(type(x))
+x=10000000000000000000000
+print(x)
+print(type(x))
+
+x=1
+print(x)
+print(type(x))
+x=10000000000000000000000
+print(x)
+print(type(x))
+
+
+
+
